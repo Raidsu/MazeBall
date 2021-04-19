929cc94b3e1a62749c15c955906c6df2fa488435
Retoree <admin@1259.msk.ru>
2021-04-19 14:45:21 +0300

    EditorWindow Added

---
+++ Assets/Editor/EditorWindows.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+
+namespace Editor
+{
+    public class EditorWindows
+    {
+        [MenuItem("My windows/Prefabs Settings")]
+        private static void MenuOption()
+        {
+            EditorWindow.GetWindow(typeof(MyEditorWindow), false, "Prefabs Settings");
+        }
+    }
+}
